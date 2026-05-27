e3938cbdcc64960008aec11ef2f70e96baa05b36
William Mead <william@m34d.com>
2026-05-27 09:26:45 +0200

    Close #164. Fixed product select on third party create card. Updated contributor details

--- pdpconnectfr/class/pdpconnectfr.class.php
+++ pdpconnectfr/class/pdpconnectfr.class.php
@@ -1,6 +1,7 @@
 <?php
 /* Copyright (C) 2025       Laurent Destailleur         <eldy@users.sourceforge.net>
  * Copyright (C) 2025       Mohamed DAOUD               <mdaoud@dolicloud.com>
+ * Copyright (C) 2026		William Mead				<william@m34d.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -1689,7 +1690,11 @@ class PdpConnectFr
 			$resprints .= '<tr class="trpdpconnect_collapseseparator trrouting_product_id '.($expand_display ? '' : 'hidden').'">';
 			$resprints .= '<td>' . $form->textwithpicto($langs->trans("DefaultProductEBilling"), $langs->trans("DefaultProductEBillingHelp")) . '</td>';
 			$resprints .= '<td'.(empty($parameters['colspanvalue']) ? '' : ' colspan="'.(((int) $parameters['colspanvalue']) -1).'"').'>';
-			$resprints .= $form->select_produits_fournisseurs($object->id, $product_id, 'routing_product_id', '', '', array(), 0, 1, '', '', 1);
+			// select_produits_fournisseurs() uses print (not return) in older Dolibarr versions; capture either way
+			ob_start();
+			$selectOut = $form->select_produits_fournisseurs($object->id, $product_id, 'routing_product_id', '', '', array(), 0, 1);
+			$captured = ob_get_clean();
+			$resprints .= ($captured !== '' && $captured !== false) ? $captured : (string) $selectOut;
 			$resprints .= '</td>';
 			$resprints .= '</tr>';
 
