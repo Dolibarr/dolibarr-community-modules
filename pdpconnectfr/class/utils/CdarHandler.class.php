22bd60dc7e4b51faf2e5c364fa9d111a8d216074
Frédéric FRANCE <frederic34@users.noreply.github.com>
2026-05-27 16:29:17 +0200

    Update CdarHandler.class.php

--- pdpconnectfr/class/utils/CdarHandler.class.php
+++ pdpconnectfr/class/utils/CdarHandler.class.php
@@ -151,7 +151,7 @@ class CdarHandler
 	 * generate
 	 *
 	 * @param  array $data array of data
-	 * @return void
+	 * @return string|false
 	 */
 	public function generate($data)
 	{
@@ -172,7 +172,7 @@ class CdarHandler
 	 *
 	 * @param  array $data array of data
 	 * @param  string $filename filename
-	 * @return void
+	 * @return bool
 	 */
 	public function saveToFile($data, $filename)
 	{
